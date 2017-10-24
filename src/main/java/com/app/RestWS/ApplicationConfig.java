9a0273625ae91a0961cd7be4956f0c98d4f8c783
josephinex <josephinex21@gmail.com>
2017-10-24 23:29:36 +0300

    initial commit

---
+++ src/main/java/com/app/RestWS/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.app.RestWS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Map<String, Object> getProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("jersey.config.server.provider.packages", "com.app.RestWS");
+        return properties;
+    }
+}
